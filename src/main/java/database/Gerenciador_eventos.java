f02555567d04297446b98ec4392e0f46ea1440d4
Gustavo Lage <lageg901@gmail.com>
2025-06-05 15:25:40 -0400

    init

---
+++ src/main/java/database/Gerenciador_eventos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package database;
+
+/**
+ *
+ * @author lageg
+ */
+public class Gerenciador_eventos {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+    }
+}
