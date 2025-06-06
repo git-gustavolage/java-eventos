9ea6ac2915f050c7ca8bd2f1d1a31c60651a1b91
Gustavo Lage <lageg901@gmail.com>
2025-06-06 17:26:59 -0400

    dao

--- src/main/java/model/dao/UserDao.java
+++ src/main/java/model/dao/UserDao.java
@@ -3,7 +3,6 @@ package model.dao;
 import database.ConnectionFactory;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
