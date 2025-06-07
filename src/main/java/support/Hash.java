a51cc896b99b0747c6c518c2f61dcba6ddd4350a
Gustavo Lage <lageg901@gmail.com>
2025-06-07 12:16:48 -0400

    Feat: classe que utiliza a lib jbcrypt para hash do password do usuario

---
+++ src/main/java/support/Hash.java
@@ -0,0 +1,18 @@
+package support;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+
+public class Hash {
+
+    public static String hash(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean verify(String password, String storedHash) {
+        if (storedHash == null || storedHash.length() == 0) {
+            return false;
+        }
+        return BCrypt.checkpw(password, storedHash);
+    }
+}
