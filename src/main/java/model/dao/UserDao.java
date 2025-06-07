52a3602d13ebbfaa872ac6d8a0b3497d4ade789a
Gustavo Lage <lageg901@gmail.com>
2025-06-07 12:22:29 -0400

    Feat: encripitando a senha do usuario antes de salva-la no banco de daods

--- src/main/java/model/dao/UserDao.java
+++ src/main/java/model/dao/UserDao.java
@@ -1,17 +1,18 @@
 package model.dao;
 
-import database.ConnectionFactory;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import database.ConnectionFactory;
 import model.bean.User;
+import support.Hash;
 
 public class UserDao {
     
     public User find(User user) {
-        
         return user;
     }
     
@@ -43,7 +44,7 @@ public class UserDao {
 
     public boolean create(User user) {
 
-        String sql = "INSERT INTO usuarios (nome, user, email, password) VALUES (?, ?, ?, ?);";
+        String sql = "INSERT INTO usuarios (nome, username, email, password) VALUES (?, ?, ?, ?)";
 
         Connection conn = ConnectionFactory.getConnection();
         PreparedStatement stmt = null;
@@ -54,11 +55,11 @@ public class UserDao {
             stmt.setString(1, user.getNome());
             stmt.setString(2, user.getUser());
             stmt.setString(3, user.getEmail());
-            stmt.setString(4, user.getPassword());
+            stmt.setString(4, Hash.hash(user.getPassword()));
 
             stmt.executeUpdate();
         } catch (SQLException ex) {
-            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, "Erro ao criar usuário!", ex);
             return false;
         } finally {
             ConnectionFactory.close(conn, stmt);
