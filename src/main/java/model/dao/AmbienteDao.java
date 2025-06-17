e96adf34fcef08d87d9ae95d60de1ebfec8b0a51
Gustavo Lage <lageg901@gmail.com>
2025-06-17 16:54:54 -0400

    Refactor: ajustando AmbienteDao para lidar usar a classe base dao

--- src/main/java/model/dao/AmbienteDao.java
+++ src/main/java/model/dao/AmbienteDao.java
@@ -1,129 +1,57 @@
 package model.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import database.ConnectionFactory;
 import model.bean.Ambiente;
 
 public class AmbienteDao {
-    
-    private static Ambiente parseAmbiente(ResultSet result) {
-        
-        try {
-            Ambiente ambiente = new Ambiente();
-            
-            Integer id = result.getInt("id");
-            String nome = result.getString("nome");
-            String descricao = result.getString("descricao");
 
-            ambiente.setId(id);
-            ambiente.setNome(nome);
-            ambiente.setDescricao(descricao);
-            
-            return ambiente;
-        } catch(SQLException e) {
-            Logger.getLogger(AmbienteDao.class.getName()).log(Level.SEVERE, "Erro ao buscar ambiente!", e);
-            return null;
-        }
+    private static Ambiente parse(ResultSet result) throws SQLException {
+        Ambiente ambiente = new Ambiente();
+        ambiente.setId(result.getInt("id"));
+        ambiente.setNome(result.getString("nome"));
+        ambiente.setDescricao(result.getString("descricao"));
+        return ambiente;
     }
-    
+
     public static Ambiente findById(int id) {
-        Connection conn = ConnectionFactory.getConnection();
-        PreparedStatement stmt = null;
-
         String sql = "SELECT * FROM ambientes WHERE id = ? LIMIT 1;";
-
-        try {
-            stmt = conn.prepareStatement(sql);
-            stmt.setInt(1, id);
-
-            ResultSet result = stmt.executeQuery();
-
-            if (!result.next()) {
-                return null;
-            }
-
-            return AmbienteDao.parseAmbiente(result);
-        } catch (SQLException ex) {
-            Logger.getLogger(AmbienteDao.class.getName()).log(Level.SEVERE, "Erro ao buscar ambiente!", ex);
-            return null;
-        } finally {
-            ConnectionFactory.close(conn, stmt);
-        }
+        return Dao.executeQueryForSingleResult(sql,
+                stmt -> stmt.setInt(1, id),
+                AmbienteDao::parse,
+                "Erro ao buscar ambiente!",
+                AmbienteDao.class);
     }
-    
+
     public static boolean create(Ambiente ambiente) {
-
         String sql = "INSERT INTO ambientes (nome, descricao) VALUES (?, ?)";
-
-        Connection conn = ConnectionFactory.getConnection();
-        PreparedStatement stmt = null;
-
-        try {
-            stmt = conn.prepareStatement(sql);
-
-            stmt.setString(1, ambiente.getNome());
-            stmt.setString(2, ambiente.getDescricao());
-
-            stmt.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(AmbienteDao.class.getName()).log(Level.SEVERE, "Erro ao criar ambiente!", ex);
-            return false;
-        } finally {
-            ConnectionFactory.close(conn, stmt);
-        }
-
-        return true;
+        return Dao.execute(sql,
+                stmt -> {
+                    stmt.setString(1, ambiente.getNome());
+                    stmt.setString(2, ambiente.getDescricao());
+                },
+                "Erro ao criar ambiente!",
+                AmbienteDao.class);
     }
-    
+
     public static boolean update(Ambiente ambiente) {
-        String sql = "UPDATE ambientes SET nome = ?, descricao = ? where id = ?";
-        
-        Connection conn = ConnectionFactory.getConnection();
-        PreparedStatement stmt = null;
-        
-        try {
-            stmt = conn.prepareStatement(sql);
-
-            stmt.setString(1, ambiente.getNome());
-            stmt.setString(2, ambiente.getDescricao());
-            stmt.setInt(3, ambiente.getId());
-
-            stmt.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(AmbienteDao.class.getName()).log(Level.SEVERE, "Erro ao atualizar ambiente!", ex);
-            return false;
-        } finally {
-            ConnectionFactory.close(conn, stmt);
-        }
-
-        return true;
+        String sql = "UPDATE ambientes SET nome = ?, descricao = ? WHERE id = ?";
+        return Dao.execute(sql,
+                stmt -> {
+                    stmt.setString(1, ambiente.getNome());
+                    stmt.setString(2, ambiente.getDescricao());
+                    stmt.setInt(3, ambiente.getId());
+                },
+                "Erro ao atualizar ambiente!",
+                AmbienteDao.class);
     }
 
     public static boolean destroy(Ambiente ambiente) {
         String sql = "DELETE FROM ambientes WHERE id = ?";
-
-        Connection conn = ConnectionFactory.getConnection();
-        PreparedStatement stmt = null;
-
-        try {
-            stmt = conn.prepareStatement(sql);
-
-            stmt.setInt(1, ambiente.getId());
-
-            stmt.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(AmbienteDao.class.getName()).log(Level.SEVERE, "Erro ao deletar ambiente!", ex);
-            return false;
-        } finally {
-            ConnectionFactory.close(conn, stmt);
-        }
-
-        return true;
+        return Dao.execute(sql,
+                stmt -> stmt.setInt(1, ambiente.getId()),
+                "Erro ao deletar ambiente!",
+                AmbienteDao.class);
     }
 }
