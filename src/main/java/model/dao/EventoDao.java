8e7da54b2e4c1a6135af59ee35bfa1a77283f80d
Erick Couto <erickcouto749@gmail.com>
2025-06-18 01:22:00 -0400

    feat: adiciona estrutura inicial do EventoDao com método parse

---
+++ src/main/java/model/dao/EventoDao.java
@@ -0,0 +1,22 @@
+package model.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.bean.Evento;
+
+public class EventoDao {
+    
+    private static Evento parse(ResultSet result) throws SQLException {
+        Evento evento = new Evento();
+        evento.setId(result.getLong("id"));
+        evento.setUuid(result.getString("uuid"));
+        evento.setTitulo(result.getString("titulo"));
+        evento.setDescricao(result.getString("descricao"));
+        evento.setDataInicio(result.getDate("data_inicio"));
+        evento.setDataTermino(result.getDate("data_termino"));
+        evento.setIdFormato(result.getLong("id_formato"));
+        evento.setCreatedAt(result.getTimestamp("created_at"));
+        evento.setUpdatedAt(result.getTimestamp("updated_at"));
+        return evento;
+    }
+}
