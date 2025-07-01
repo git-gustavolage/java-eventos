e610d21f6cf3d4d90fdf764748759affa920a390
Kassandra-Olv25 <kassandra.olv.rodrigues@gmail.com>
2025-06-30 20:04:19 -0400

    Style: adicionando prefixos as variaveis.

--- src/main/java/view/Tela_login.java
+++ src/main/java/view/Tela_login.java
@@ -32,33 +32,22 @@ public class Tela_login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jTextField1 = new javax.swing.JTextField();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextPane1 = new javax.swing.JTextPane();
-        jLabel3 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
-        lbl_EVENTOS = new javax.swing.JLabel();
+        lbl_eventos = new javax.swing.JLabel();
         btn__entrar = new javax.swing.JButton();
         pwd_senhausuario = new javax.swing.JPasswordField();
         txt_nomeusuario = new javax.swing.JTextField();
-        lbl_npcad = new javax.swing.JLabel();
+        lbl_notcadastro = new javax.swing.JLabel();
         lbl_cadastrase = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
-
-        jTextField1.setText("jTextField1");
-
-        jScrollPane1.setViewportView(jTextPane1);
-
-        jLabel3.setFont(new java.awt.Font("Franklin Gothic Medium", 0, 14)); // NOI18N
-        jLabel3.setText("Cadastra-se");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
-        lbl_EVENTOS.setFont(new java.awt.Font("Franklin Gothic Medium", 0, 32)); // NOI18N
-        lbl_EVENTOS.setText("EVENTOS");
-        lbl_EVENTOS.setToolTipText("");
+        lbl_eventos.setFont(new java.awt.Font("Franklin Gothic Medium", 0, 32)); // NOI18N
+        lbl_eventos.setText("EVENTOS");
+        lbl_eventos.setToolTipText("");
 
         btn__entrar.setBackground(new java.awt.Color(0, 212, 146));
         btn__entrar.setFont(new java.awt.Font("Franklin Gothic Medium", 0, 14)); // NOI18N
@@ -91,8 +80,8 @@ public class Tela_login extends javax.swing.JFrame {
             }
         });
 
-        lbl_npcad.setFont(new java.awt.Font("Franklin Gothic Medium", 0, 14)); // NOI18N
-        lbl_npcad.setText("Não possui cadastro? ");
+        lbl_notcadastro.setFont(new java.awt.Font("Franklin Gothic Medium", 0, 14)); // NOI18N
+        lbl_notcadastro.setText("Não possui cadastro? ");
 
         lbl_cadastrase.setFont(new java.awt.Font("Franklin Gothic Medium", 0, 14)); // NOI18N
         lbl_cadastrase.setForeground(new java.awt.Color(43, 127, 255));
@@ -110,13 +99,13 @@ public class Tela_login extends javax.swing.JFrame {
                 .addContainerGap())
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(lbl_npcad)
+                .addComponent(lbl_notcadastro)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lbl_cadastrase)
                 .addGap(0, 0, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(96, Short.MAX_VALUE)
-                .addComponent(lbl_EVENTOS)
+                .addComponent(lbl_eventos)
                 .addGap(96, 96, 96))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(120, 120, 120)
@@ -127,14 +116,14 @@ public class Tela_login extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(29, 29, 29)
-                .addComponent(lbl_EVENTOS)
+                .addComponent(lbl_eventos)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
                 .addComponent(txt_nomeusuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(pwd_senhausuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbl_npcad)
+                    .addComponent(lbl_notcadastro)
                     .addComponent(lbl_cadastrase))
                 .addGap(18, 18, 18)
                 .addComponent(btn__entrar)
@@ -144,7 +133,7 @@ public class Tela_login extends javax.swing.JFrame {
         pwd_senhausuario.getAccessibleContext().setAccessibleName("campo_senha");
         pwd_senhausuario.getAccessibleContext().setAccessibleParent(pwd_senhausuario);
         txt_nomeusuario.getAccessibleContext().setAccessibleName("campo_usuario");
-        txt_nomeusuario.getAccessibleContext().setAccessibleParent(lbl_EVENTOS);
+        txt_nomeusuario.getAccessibleContext().setAccessibleParent(lbl_eventos);
 
         jPanel2.setBackground(new java.awt.Color(43, 127, 255));
         jPanel2.setForeground(new java.awt.Color(43, 127, 255));
@@ -254,15 +243,11 @@ public class Tela_login extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn__entrar;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextPane jTextPane1;
-    private javax.swing.JLabel lbl_EVENTOS;
     private javax.swing.JLabel lbl_cadastrase;
-    private javax.swing.JLabel lbl_npcad;
+    private javax.swing.JLabel lbl_eventos;
+    private javax.swing.JLabel lbl_notcadastro;
     private javax.swing.JPasswordField pwd_senhausuario;
     private javax.swing.JTextField txt_nomeusuario;
     // End of variables declaration//GEN-END:variables
